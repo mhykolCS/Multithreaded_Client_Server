ed1d839f72fc7f393c2595b9dd4d3952b0579996
Michael <michaelsand155@gmail.com>
2023-10-01 18:39:24 +1000

    shared memory boilerplate

--- client.c
+++ client.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#define BLOCK_SIZE 352
 
 int main(int argc, char** argv[]){
 
-    printf("Hello Client!");
+    key_t key = ftok("sh_mem.key", 0);
+    if(key == -1){
+        printf("Error reading 'sh_mem.key' file\n");
+        return(0);
+    }
+
+    int block_id = shmget(key, BLOCK_SIZE, 0666 | IPC_CREAT);
+    if(block_id == -1){
+        printf("Error creating block ID\n");
+        return(0);
+    }
+
+    char* sh_ptr = shmat(block_id, NULL, 0);
+    if(sh_ptr == (char *)-1){
+        printf("Error binding block to address space\n");
+        return(0);
+    }
+
+    printf("%s\n", sh_ptr);
 
     return(0);
 }
