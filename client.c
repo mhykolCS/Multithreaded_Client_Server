26c78f21fa4c2618510c409012439245f549fcd4
Michael <michaelsand155@gmail.com>
2023-10-02 19:04:03 +1000

    Had wrong idea, starting from scratch

--- client.c
+++ client.c
@@ -3,12 +3,6 @@
 
 int main(int argc, char** argv[]){
 
-    struct client_data{
-        unsigned long number;
-        char text[32];
-        char* str_ptr;
-    } user_input;
-
     key_t key = get_file_key();
     if(key == 0){
         return(0);
@@ -20,24 +14,6 @@ int main(int argc, char** argv[]){
     }
 
     
-    while(1){
-        printf("Please enter a number to be factored: ");
-        scanf("%32s", user_input.text);
-
-        user_input.number = strtoul(user_input.text, &user_input.str_ptr, 10);
-
-        if(user_input.number == 0){
-            if(user_input.text[0] == 'q' || user_input.text[0] == 'Q'){
-                break;
-            }
-        }
-
-        printf("%ld\n", user_input.number);
-        
-        
-
-    }
-
 
     shmctl(key, IPC_RMID, NULL);
 
--- server.c
+++ server.c
@@ -1,38 +1,8 @@
 #include "shm_func.h"
-#include <pthread.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-void* factorise(void* arg){
-
-    unsigned long* input = (unsigned long*) arg;
-    unsigned long* result = (unsigned long*) malloc(sizeof(unsigned long));
-
-    *result = *input * (unsigned long)5;
-
-    return(result);
-}
 
 
 
 int main(int argc, char** argv[]){
-
-    struct server_data{
-        pthread_t thread;
-        unsigned long input;
-        unsigned long* result;
-    } data[10];
-
-    data[0].input = 5;
-
-    pthread_create(&data[0].thread, NULL, factorise, &data[0].input);
-    pthread_join(data[0].thread, (void*)&data[0].result);
-
-    printf("%ld\n", *data[0].result);
-    
-
-
-    
 
     key_t key = get_file_key();
     if(key == 0){
@@ -44,8 +14,7 @@ int main(int argc, char** argv[]){
         return(0);
     }
 
-    sh_ptr[0] = 7354;
-    sh_ptr[1] = 11111195;
+    
     
     shmctl(key, IPC_RMID, NULL);
 
